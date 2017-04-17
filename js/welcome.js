9cda503d45ce49e6f85a73abfd072cf3926fadd8
Janine Villareal <JV@JANINESCBOOKPRO.hitronhub.home>
2017-04-17 08:26:11 -0600

    Overlay Nav and minor changes
    1.) Added the overlay nav, there are issues with it though
    2.) fixed naming conventions

--- js/welcome.js
+++ js/welcome.js
@@ -5,3 +5,49 @@
  */
 
 
+$(function() {
+  //chache DOM Elements
+  var $overlayNav = $('.flex-overlay');
+  var $nav = $('.flex-nav');
+  var $hamburger = $('#hamburger');
+  var $cover = $('#cover');
+
+  var $panels = $('#home, #about, #projects, #contact');
+  var $panelTitles = $('#home .titles, #about .titles, #projects .titles, #contact .titles');
+  var $hiddenDescription = $('.hidden-description');
+
+  /*Hide overlay on load*/
+  $overlayNav.add($panels).add($panelTitles).add($hiddenDescription).toggle();
+
+  function panelHover() {
+    $('#home').hover(function() {
+      $(this).find('.hidden-description').fadeToggle();
+    });
+    $('#about').hover(function() {
+      $(this).find('.hidden-description').fadeToggle();
+    });
+    $('#projects').hover(function() {
+      $(this).find('.hidden-description').fadeToggle();
+    });
+    $('#contact').hover(function() {
+      $(this).find('.hidden-description').fadeToggle();
+    });
+  }
+
+  function hamburgerClicked() {
+    //hide original nav and show overlay
+    $nav.add($cover).toggle();
+    $overlayNav.toggle();
+    //flip each column panel and then show text
+    $panels.toggle().addClass('animated flipInY');
+    $panelTitles.delay(4000).toggle().addClass('animated fadeInDown');
+    /* IMPORTANT NOTE:
+    Find a better way to get hidden description to slide in on panel hover and take out the repetition in code*/
+    panelHover();
+  }
+  
+  $hamburger.click(function() {
+    hamburgerClicked();
+  });
+
+});
